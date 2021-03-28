ae0235cf8e0791ff7f484789d9d57ee16b02bdbd
Iryna Protsenko <irene.protsenko@gmail.com>
2021-03-29 02:12:48 +0300

    friendlist - done

--- src/components/FriendsList/FriendListItem.jsx
+++ src/components/FriendsList/FriendListItem.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React from 'react';
 
-const FriendListItem = ({ friend }) => (<li class="item">
-    <span class="status">{friend.isOnline}</span>
-    <img class="avatar" src={friend.avatar} alt={friend.name} width="48" />
-    <p class="name">{friend.name}</p>
-</li>);
-export default FriendListItem
+import Styles from './friendsListStyles.module.css';
+const status = status => {
+  return status ? Styles.statusGreen : Styles.statusRed;
+};
+const FriendListItem = ({ friend }) => (
+  <li className={Styles.item}>
+    <span className={status(friend.isOnline)}>{friend.isOnline}</span>
+    <img
+      className={Styles.avatar}
+      src={friend.avatar}
+      alt={friend.name}
+      width="48"
+    />
+    <p className={Styles.name}>{friend.name}</p>
+  </li>
+);
+export default FriendListItem;
